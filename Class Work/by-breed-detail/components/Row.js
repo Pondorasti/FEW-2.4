f3f3f1e1b74704b49b3948e5549c1d77b5fcb7db
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-02-16 00:10:16 -0800

    feat: pressable

--- Class Work/by-breed-detail/components/Row.js
+++ Class Work/by-breed-detail/components/Row.js
@@ -1,8 +1,8 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View, Text, Pressable } from "react-native"
 import tw from "twrnc"
 
-export default function Item({ animal }) {
+export default function Row({ animal }) {
   let rating = 0
   for (const key in animal) {
     if (!isNaN(animal[key])) {
@@ -13,7 +13,17 @@ export default function Item({ animal }) {
   rating = Math.round(rating)
 
   return (
-    <View style={tw`my-2 mx-4 p-3 bg-gray-50 rounded-xl`}>
+    <Pressable
+      style={tw`my-2 mx-4 p-3 bg-gray-50 rounded-xl`}
+      style={({ pressed }) =>
+        tw.style(
+          "my-2 mx-4 p-3 rounded-xl",
+          pressed ? "bg-gray-100" : "bg-gray-50",
+          pressed ? "border border-gray-300" : "border border-gray-200"
+        )
+      }
+      onPress={() => console.log("hello")}
+    >
       <View style={tw`flex flex-row justify-between items-center mb-1`}>
         <Text style={tw`font-600 text-lg text-black`}>{animal.breed}</Text>
         <Text style={tw`text-yellow-500 text-lg`}>{"✦".repeat(rating)}</Text>
@@ -25,6 +35,13 @@ export default function Item({ animal }) {
       <Text style={tw`text-black`}>
         • Playfulness: {animal["Potential for Playfulness"] || "∅"}
       </Text>
-    </View>
+    </Pressable>
   )
 }
+
+// [
+//   {
+//     backgroundColor: pressed ? "rgb(210, 230, 255)" : "white",
+//   },
+//   styles.wrapperCustom,
+// ]
