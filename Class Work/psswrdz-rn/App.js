c019a7b8ebc2ed350ba9aacce8ed117be312e78b
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-02-28 18:28:54 -0800

    feat(psswrdz-rn): home list

--- Class Work/psswrdz-rn/App.js
+++ Class Work/psswrdz-rn/App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { SafeAreaView, FlatList, Pressable } from "react-native"
+import { createStackNavigator } from "@react-navigation/stack"
+import { NavigationContainer } from "@react-navigation/native"
+import tw from "twrnc"
+import { Feather } from "@expo/vector-icons"
+import Row from "./components/Row"
+
+export function Home({ navigation }) {
+  return (
+    <SafeAreaView>
+      <StatusBar style="auto" />
+      {/* <FlatList
+        style={tw`bg-gray-200 py-2`}
+        data={cats}
+        renderItem={({ item }) => <Row animal={item} navigation={navigation} />}
+        keyExtractor={(item) => item.breed}
+      /> */}
+
+      <Row navigation={navigation} />
+      <Row navigation={navigation} />
+      <Row navigation={navigation} />
+    </SafeAreaView>
+  )
+}
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Passwords"
+          component={Home}
+          options={{
+            headerRight: () => (
+              <Pressable onPress={() => alert("This is a button!")}>
+                <Feather name="plus" size={24} style={tw`text-gray-400 mr-4`} />
+              </Pressable>
+            ),
+          }}
+        />
+        {/* <Stack.Screen
+          name="Detail"
+          component={Detail}
+          style={tw`bg-gray-200 py-2`}
+          options={({ route }) => ({ title: route.params.animal.breed })}
+        /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Class Work/psswrdz-rn/components/Row.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { View, Text, Pressable } from "react-native"
+import tw from "twrnc"
+import { Feather } from "@expo/vector-icons"
+
+export default function Row({ navigation }) {
+  return (
+    <Pressable
+      style={tw`my-2 mx-4 p-3 bg-gray-50 rounded-xl`}
+      style={({ pressed }) =>
+        tw.style(
+          "my-2 mx-4 p-3 rounded-xl",
+          pressed ? "bg-gray-100" : "bg-gray-50",
+          pressed ? "border border-gray-300" : "border border-gray-200"
+        )
+      }
+      onPress={() => navigation.navigate("Detail", {})}
+    >
+      <View style={tw`flex flex-row items-center`}>
+        <Text style={tw`font-600 text-lg text-black`}>Vercel</Text>
+        <View style={tw`flex-grow`}></View>
+        <Feather name="chevron-right" size={24} style={tw`text-gray-400`} />
+      </View>
+    </Pressable>
+  )
+}
